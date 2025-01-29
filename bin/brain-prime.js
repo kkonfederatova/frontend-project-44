cc51547823654a9f14f500af6a68977a836e765d
kkonfederatova <kseniya.konfederatova@gmail.com>
2025-01-29 17:44:00 +0200

    add function brainPrime()

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -2,34 +2,9 @@
 import readlineSync from 'readline-sync';
 
 import {
-  askName, getRandomNumber, isNumberPrime,
+  askName, brainPrime,
   // eslint-disable-next-line import/extensions
 } from '../src/index.js';
 
-let answer;
-let number;
-
 const name = askName();
-console.log('Answer "yes" if given number is prime. Otherwise answer "no".\n');
-
-let i = 1;
-let isAnswerTrue = true;
-while (isAnswerTrue && i < 4) {
-  number = getRandomNumber();
-  console.log(`Question: ${number}`);
-  answer = readlineSync.question('Your answer: ');
-
-  if (isNumberPrime(number) === answer) {
-    console.log('Correct!');
-    isAnswerTrue = true;
-    i += 1;
-  } else {
-    console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
-    console.log(`Let's try again, ${name}!`);
-    isAnswerTrue = false;
-  }
-}
-
-if (isAnswerTrue === true) {
-  console.log(`Congratulations, ${name}!`);
-}
+brainPrime(name);
--- src/index.js
+++ src/index.js
@@ -186,3 +186,31 @@ export const brainProgression = (name) => {
     console.log(`Congratulations, ${name}!`);
   }
 };
+
+export const brainPrime = (name) => {
+  let answer;
+  let number;
+  let i = 1;
+  let isAnswerTrue = true;
+
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+  while (isAnswerTrue && i < 4) {
+    number = getRandomNumber();
+    console.log(`Question: ${number}`);
+    answer = readlineSync.question('Your answer: ');
+
+    if (isNumberPrime(number) === answer) {
+      console.log('Correct!');
+      isAnswerTrue = true;
+      i += 1;
+    } else {
+      console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
+      console.log(`Let's try again, ${name}!`);
+      isAnswerTrue = false;
+    }
+  }
+
+  if (isAnswerTrue === true) {
+    console.log(`Congratulations, ${name}!`);
+  }
+};
