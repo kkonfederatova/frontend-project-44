d87a13b4657b04c39a86565148ceaf6347c05c4a
kkonfederatova <kseniya.konfederatova@gmail.com>
2025-01-29 17:00:07 +0200

    fix export default askName

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 // eslint-disable-next-line import/extensions
-import { askName } from '../src/cli.js';
+import askName from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 askName();
